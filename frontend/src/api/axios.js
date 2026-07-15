43de58880333322fb02d15355938974105c80c1c
Samreet Kaur <samreet205@gmail.com>
2026-07-15 20:16:17 +0530

    some fixing

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,12 @@
 import axios from 'axios';
 
+const rawApiUrl = import.meta.env.VITE_API_URL;
+const normalizedApiUrl = rawApiUrl
+  ? `${rawApiUrl.replace(/\/+$|\/api$/g, '').replace(/\/$/, '')}/api`
+  : 'http://localhost:5000/api';
+
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api',
+  baseURL: normalizedApiUrl,
   withCredentials: true,
 });
 
